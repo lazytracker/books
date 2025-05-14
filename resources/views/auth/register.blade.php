ffac3cbcf3f3b621e681fb1233ff3f0e5b835266
lazytracker <lazytracker@gmail.com>
2025-05-14 14:25:59 +0300

    2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,12 @@
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
+        <!-- INN -->
+        <div class="mt-4">
+            <x-input-label for="inn" :value="__('ИНН')" />
+            <x-text-input id="inn" class="block mt-1 w-full" type="text" name="inn" :value="old('inn')" required pattern="\d{10}" title="ИНН должен содержать ровно 10 цифр" />
+            <x-input-error :messages="$errors->get('inn')" class="mt-2" />
+        </div>
 
         <!-- Email Address -->
         <div class="mt-4">
