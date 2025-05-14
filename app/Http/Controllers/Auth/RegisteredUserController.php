a7d48f4960ccde5c39afe1bf1524e18e92aff394
lazytracker <lazytracker@gmail.com>
2025-05-14 15:40:26 +0300

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'inn' => $request->inn,
         ]);
 
         event(new Registered($user));
